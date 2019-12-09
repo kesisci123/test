027827c542342124c742eece9a24deebe1efd32f
kesisci123 <anomhali@gmail.com>
2019-12-09 19:50:54 +0300

    chat added

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,4 @@
+
 let socket = io();
 socket.on('connect', function(){
     console.log("Connected to server.");
@@ -9,4 +10,20 @@ socket.on('disconnect', function(){
 
 socket.on('newMessage', function(message){
     console.log("newMessage", message);
+    let li = document.createElement('li');
+    li.innerText = `${message.from} : ${message.text}`;
+    document.querySelector('body').appendChild(li);
+}, function(message){
+    console.log("Do you Got it", message);
+});
+
+
+document.querySelector('#submit_bts').addEventListener("click", function(e){
+    e.preventDefault();
+    socket.emit('createMessage', {
+        from : "User",
+        text : document.querySelector('input[name="message"]').value
+    }, function(){
+
+    });
 });
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const socketIo = require('socket.io');
 const http = require("http");
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, "./../public");
 const port = process.env.PORT || 3000;
@@ -16,27 +17,16 @@ io.on('connection', (socket) =>{
     console.log("A new user just connected.");
 
     //To new joined user
-    socket.emit('newMessage',{
-        from:"Admin",
-        text:"Welcome to the chat app",
-        createdAt: new Date().getTime()
-    });
+    socket.emit('newMessage', generateMessage("Admin", 'Welcome to the chat app.'));
 
-    socket.broadcast.emit('newMessage',{
-        from:"Admin",
-        text:"New User Joined",
-        createdAt: new Date().getTime()
-    });
+
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined'));
    
 
-    socket.on('createMessage', (message)=>{
+    socket.on('createMessage', (message, callback)=>{
         console.log("createMessage :",message);
-        io.emit('newMessage',{
-            from: message.from,
-            text:message.text,
-            createdAt : new Date().getTime()
-        });
-       
+        io.emit('newMessage',generateMessage(message.from, message.text));
+        callback && callback('Yes');
     });
     
     socket.on('disconnect', () =>{
